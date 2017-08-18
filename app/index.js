2aa1024b549c33fef455705007f8a844927c8e89
jerryxu <13048699388@163.com>
2017-08-19 02:16:34 +0800

    更新脚手架

--- app/index.js
+++ app/index.js
@@ -4,21 +4,52 @@ var util = require('util');
 var Generator = require('yeoman-generator');
 var yosay = require('yosay');    //yeoman弹出框
 var path = require('path');
-module.exports =  class extends Generator {
+default class extends Generator {
     constructor(args, opts) {
         // Calling the super constructor is important so our generator is correctly set up
         super(args, opts);
 
         // Next, add your custom code
+        this.option('framework', {type: String, required: true});
+        this.option('modules', {type: String, required: false});
         this.option('babel'); // This method adds support for a `--babel` flag
     }
 
     prompting() {
-        return this.prompt([{
+        return this.prompt([
+            {
+                when: !this.options.framework,
+                type: 'list',
+                name: 'framework',
+                message: 'Which JavaScript framework do you want?',
+                choices: [
+                    {name: 'React', value: 'react'},
+                    {name: 'Angular 2', value: 'vue'},
+                ]
+            },
+            {
+                when(responses) {
+                    if (responses.framework === 'vue') {
+                        responses.modules = 'vue';
+                        return false;
+                    }
+                    return !responses.modules && !modules;
+                },
+            },
+            {
+                when(responses) {
+                    if (responses.framework === 'react') {
+                        responses.modules = 'react';
+                        return false;
+                    }
+                    return !responses.modules && !modules;
+                },
+            },
+            {
             type    : 'input',
             name    : 'name',
             message : '您的项目名称',
-        }
+            }
         // , {
         //     type    : 'confirm',
         //     name    : 'cool',
@@ -30,28 +61,18 @@ module.exports =  class extends Generator {
             message : '项目开发者名称',
         }
         ]).then((answers) => {
-            this.appname = answers.name
-            this.author = answers.author
+            this.appname = answers.name;
+            this.author = answers.author;
+            this.framework = answers.framework;
+            this.modules = answers.modules;
         });
 
     }
 
     writing() {
         this.fs.copy(
-            this.templatePath('all'),
+            this.templatePath(this.framework),
             this.destinationPath(`${this.appname}`)
-        );
-        this.fs.copy(
-            this.templatePath('all/.*'),
-            this.destinationPath(`${this.appname}`)
-        );
-        this.fs.copyTpl(
-            this.templatePath('package.json'),
-            this.destinationPath(`${this.appname}/package.json`),
-            {
-                name: this.appname,
-                author: this.author
-            }
         );
         this.sourceRoot(`./${this.appname}`);
     }
