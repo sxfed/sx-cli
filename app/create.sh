f428822e27bdccd0fab4099ccdbefb6391c798d0
jerryxu <13048699388@163.com>
2017-08-22 02:54:11 +0800

    [shell]设置别名

--- app/create.sh
+++ app/create.sh
@@ -1,7 +1,14 @@
 #!/bin/bash --login
 
-alias Sxstart="yarn run dev"
+unalias sxinit
+unalias sxstart
+unalias sxstop
+
+echo 'alias sxinit="yo sx-cli"' >> ~/.bashrc
+echo 'alias sxstart="yarn run dev"' >> ~/.bashrc
+echo 'alias sxstop="yarn run stop"' >> ~/.bashrc
 
 shopt -s  expand_aliases
 shopt expand_aliases
-sxstart
+
+
